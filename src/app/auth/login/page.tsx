31784b3cd7b2fb7afdfd167361b85b93cb8d162f
toastycoder0 <re-jos2002@proton.me>
2026-08-21 05:51:33 -0600

    feat: implement LoginPage component with login form

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,11 +1,49 @@
 import type { Metadata } from 'next';
+import Link from 'next/link';
+import { LoginForm } from '@/components/forms/login-form';
+import { env } from '@/env';
 
 export const metadata: Metadata = {
   title: 'Iniciar sesión',
 };
 
-function Login() {
-  return <h1>Login</h1>;
-}
+export default function LoginPage() {
+  const whatsappMessage = encodeURIComponent(
+    'Hola, mucho gusto. Me gustaría obtener información sobre cómo acceder a la plataforma de https://www.boya.com.mx',
+  );
 
-export default Login;
+  return (
+    <article className='mx-auto max-w-md w-full'>
+      <header className='mb-5 text-center'>
+        <h1 className='font-semibold text-2xl text-neutral-900'>Iniciar sesión en Boya</h1>
+        <p className='mt-3 text-sm text-neutral-500'>
+          La plataforma líder para distribuidores y contratistas. Accede a nuestro catálogo y
+          gestiona tus pedidos al instante.
+        </p>
+      </header>
+
+      <LoginForm />
+
+      <Link
+        href='/auth/forgot-password'
+        className='mt-5 text-end text-sm block font-medium text-slate-950 outline-none hover:underline focus-visible:underline'
+        aria-label='Recuperar contraseña olvidada'
+      >
+        ¿Olvidaste tu contraseña?
+      </Link>
+
+      <p className='mt-9 text-center text-sm text-neutral-500'>
+        ¿No tienes una cuenta?{' '}
+        <Link
+          href={`https://api.whatsapp.com/send?phone=${env.NEXT_PUBLIC_PHONE}&text=${whatsappMessage}`}
+          className='font-medium text-slate-950 outline-none hover:underline focus-visible:underline'
+          target='_blank'
+          rel='noopener noreferrer'
+          aria-label='Contáctanos vía WhatsApp para obtener información sobre cómo acceder a la plataforma'
+        >
+          Contáctanos
+        </Link>
+      </p>
+    </article>
+  );
+}
