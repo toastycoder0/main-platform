900e5e2724b4748bc40c3685d743186784d9d423
toastycoder0 <re-jos2002@proton.me>
2026-08-21 02:26:40 -0600

    feat: add login page with metadata and basic structure

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Iniciar sesión',
+};
+
+function Login() {
+  return <h1>Login</h1>;
+}
+
+export default Login;
