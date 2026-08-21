226cceb1a5aed7a9ce73efbb512a8ece128346cd
toastycoder0 <re-jos2002@proton.me>
2026-08-20 23:27:17 -0600

    feat: added root drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'drizzle-kit';
+import { env } from '@/env';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/lib/db/schema',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+});
