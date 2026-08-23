3d77dd3bc7673ccbd549460049fe421b1d89a1e9
toastycoder0 <re-jos2002@proton.me>
2026-08-22 23:51:32 -0600

    feat: create shared auth instance

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,4 @@
 import { toNextJsHandler } from 'better-auth/next-js';
-import { auth } from '@/modules/auth/infrastructure';
+import { auth } from '@/shared/auth';
 
 export const { GET, POST } = toNextJsHandler(auth);
