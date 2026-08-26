70cfe5f8353c8abc9ef942877a9fd39888757749
toastycoder0 <re-jos2002@proton.me>
2026-08-25 21:55:48 -0600

    fix: added explicit drizzle schemas reference

--- src/shared/auth/index.ts
+++ src/shared/auth/index.ts
@@ -2,10 +2,12 @@ import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { admin } from 'better-auth/plugins';
 import { db } from '@/shared/db';
+import { account, session, user, verification } from '@/shared/db/schema';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg',
+    schema: { user, session, account, verification },
   }),
   user: {
     modelName: 'user',
