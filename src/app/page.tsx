a44c6eb74a7623ed1b40c402016d7efae9b08d1a
toastycoder0 <re-jos2002@proton.me>
2026-08-26 09:06:13 -0600

    feat: implement session management and logout functionality in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-function Home() {
-  return <h1>Hello World</h1>;
+import { headers } from 'next/headers';
+import Link from 'next/link';
+import { logout } from '@/modules/auth/infrastructure/auth.action';
+import { auth } from '@/shared/auth';
+import { Button, buttonVariants } from '@/shared/components/button';
+
+async function Home() {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  return (
+    <div className='p-8'>
+      {session ? (
+        <form action={logout}>
+          <Button type='submit'>Cerrar sesión</Button>
+        </form>
+      ) : (
+        <Link className={buttonVariants()} href='/auth/login'>
+          Iniciar sesión
+        </Link>
+      )}
+    </div>
+  );
 }
 
 export default Home;
