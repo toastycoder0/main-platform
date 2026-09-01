13b2e7e7c9d50e792749d842ac627783e46df1b7
toastycoder0 <re-jos2002@proton.me>
2026-09-01 17:21:19 -0600

    feat: update seed function to create admin user with email and password from environment variables

--- src/shared/seed/index.ts
+++ src/shared/seed/index.ts
@@ -1,9 +1,20 @@
-import { env } from "@/config/env";
-import { auth } from "@/shared/auth";
+import { env } from '@/config/env';
+import { auth } from '@/shared/auth';
 
 async function seed() {
-  const { AUTH_EMAIL = "", AUTH_PASSWORD = "" } = env;
+  const { ADMIN_SEED_EMAIL: email, ADMIN_SEED_PASSWORD: password } = env;
+
+  const newUser = await auth.api.createUser({
+    body: {
+      email,
+      password,
+      role: 'admin',
+      name: 'John',
+      data: {
+        lastName: 'Doe',
+      },
+    },
+  });
 }
 
 await seed();
-
